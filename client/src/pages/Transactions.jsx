cd1c92469e1d79c29d03ffb52db69b4f517f8f78
chetanmanoj <chetanmanoj2001@gmail.com>
2024-05-25 10:04:06 +0400

    Final commit

--- client/src/pages/Transactions.jsx
+++ client/src/pages/Transactions.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { dash } from "../assets";
 
 const Transactions = () => {
   return (
-    <div>Transactions</div>
+    <div className="bg-[#EEEFFF] p-8 flex flex-col gap-6 font-gilroy">
+    <div className="flex flex-row">
+      <div className="flex flex-row gap-4 justify-center text-[#707895] text-[16px] font-semibold">
+        <img src={dash} alt="dash icon" />
+        <h1 className="text-[#707895] font-bold">TRADES TRANSACTIONS</h1>
+      </div>
+      
+    </div>
+   
+  </div>
   )
 }
 
